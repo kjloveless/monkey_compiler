8d9522311d74c06ab12301d78a52e330efa66097
Kyle Loveless <kjloveless321@gmail.com>
2024-05-09 09:27:09 -0500

    begin w/ interpreter project

---
+++ token/token.go
@@ -0,0 +1,71 @@
+package token
+
+const (
+    ILLEGAL = "ILLEGAL"
+    EOF     = "EOF"
+
+    // Identifiers + literals
+    IDENT   = "IDENT"   // add, foobar, x, y, ...
+    INT     = "INT"     // 123456
+
+    // Operators
+    ASSIGN      = "="
+    PLUS        = "+"
+    MINUS       = "-"
+    BANG        = "!"
+    ASTERISK    = "*"
+    SLASH       = "/"
+
+    EQ      = "=="
+    NOT_EQ  = "!="
+
+    LT = "<"
+    GT = ">"
+
+    // Delimiters
+    COMMA       = ","
+    SEMICOLON   = ";"
+    COLON       = ":"
+
+    LPAREN      = "("
+    RPAREN      = ")"
+    LBRACE      = "{"
+    RBRACE      = "}"
+    LBRACKET    = "["
+    RBRACKET    = "]"
+
+    // Keywords
+    FUNCTION    = "FUNCTION"
+    LET         = "LET"
+    TRUE        = "TRUE"
+    FALSE       = "FALSE"
+    IF          = "IF"
+    ELSE        = "ELSE"
+    RETURN      = "RETURN"
+
+    STRING      = "STRING"
+)
+
+var keywords = map[string]TokenType{
+    "fn":       FUNCTION,
+    "let":      LET,
+    "true":     TRUE,
+    "false":    FALSE,
+    "if":       IF,
+    "else":     ELSE,
+    "return":   RETURN,
+}
+
+func LookupIdent(ident string) TokenType {
+    if tok, ok := keywords[ident]; ok {
+        return tok
+    }
+    return IDENT
+}
+
+type TokenType string
+
+type Token struct {
+    Type TokenType
+    Literal string
+}
