53a2a8961bdf530c590843e935effc51ce2cc5a8
Kyle Loveless <kjloveless321@gmail.com>
2024-05-16 16:25:58 -0500

    compile local bindings

--- code/code.go
+++ code/code.go
@@ -51,6 +51,9 @@ var definitions = map[Opcode]*Definition{
     OpCall:         {"OpCall", []int{}},
     OpReturnValue:  {"OpReturnValue", []int{}},
     OpReturn:       {"OpReturn", []int{}},
+
+    OpGetLocal:     {"OpGetLocal", []int{1}},
+    OpSetLocal:     {"OpSetLocal", []int{1}},
 }
 
 func Lookup(op byte) (*Definition, error) {
@@ -98,11 +101,16 @@ const (
     OpCall
     OpReturnValue
     OpReturn
+
+    OpGetLocal
+    OpSetLocal
 )
 
 func ReadUint16(ins Instructions) uint16 {
     return binary.BigEndian.Uint16(ins)
 }
+
+func ReadUint8(ins Instructions) uint8 { return uint8(ins[0]) }
 
 func (ins Instructions) String() string {
     var out bytes.Buffer
@@ -150,6 +158,9 @@ func ReadOperands(def *Definition, ins Instructions) ([]int, int) {
         switch width {
         case 2:
             operands[i] = int(ReadUint16(ins[offset:]))
+
+        case 1:
+            operands[i] = int(ReadUint8(ins[offset:]))
         }
 
         offset += width
@@ -178,6 +189,8 @@ func Make(op Opcode, operands ...int) []byte {
         switch width {
         case 2:
             binary.BigEndian.PutUint16(instruction[offset:], uint16(o))
+        case 1:
+            instruction[offset] = byte(o)
         }
         offset += width
     }
